ca6c2b619fe20a6d9681eefa8275e59ece6a939e
nicola <nicolamorelli30008@gmail.com>
2025-06-08 12:33:47 +0200

    .

--- controllers/settings_controller.py
+++ controllers/settings_controller.py
@@ -123,38 +123,3 @@ class SettingsController(QObject):
         self.model.update_notification_settings(settings)
 
 
-# MAIN WIDGET (Facade)
-class SettingsWidget(QWidget):
-    """Main Settings Widget - acts as a facade maintaining the original interface"""
-    
-    def __init__(self, parent=None, flags=None):
-        super().__init__(parent)
-        
-        # Create MVC components
-        self.model = SettingsModel()
-        self.view = SettingsView(self)
-        self.controller = SettingsController(self.model, self.view)
-        
-        # Setup layout to contain the view
-        layout = QVBoxLayout(self)
-        layout.setContentsMargins(0, 0, 0, 0)
-        layout.addWidget(self.view)
-        
-        # Expose buttons for external access (maintaining original interface)
-        self.save_appearance_btn = self.view.save_appearance_btn
-        self.connection_settings_btn = self.view.connection_settings_btn  
-        self.save_button = self.view.save_button
-        
-        # Expose other necessary attributes for compatibility
-        self.cluster_address = self.view.cluster_address
-        self.username = self.view.username
-        self.password = self.view.password
-        self.jobs_queue_options_group = self.view.jobs_queue_options_group
-    
-    def get_notification_settings(self):
-        """Get notification settings - maintains original interface"""
-        return self.controller.get_notification_settings()
-    
-    def set_notification_settings(self, settings):
-        """Set notification settings - maintains original interface"""
-        self.controller.set_notification_settings(settings)
